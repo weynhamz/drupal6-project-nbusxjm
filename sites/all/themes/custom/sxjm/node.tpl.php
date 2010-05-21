9bd7bd8bd8dc5c94ac5b47c51ae2f9f9ece6d67a
Techlive Zheng <techlivezheng@gmail.com>
2010-05-21 10:10:26 +0800

    Add node teaser.

--- sites/all/themes/custom/sxjm/node.tpl.php
+++ sites/all/themes/custom/sxjm/node.tpl.php
@@ -48,7 +48,7 @@
  * @see template_preprocess_node()
  */
 ?>
-<div id="node-<?php print $node->nid; ?>" class="node<?php if ($sticky) { print ' sticky'; } ?><?php if (!$status) { print ' node-unpublished'; } ?>">
+<div id="node-<?php print $node->nid; ?>" class="node<?php if ($sticky) { print ' sticky'; } ?><?php if ($teaser) { print ' teaser'; } ?><?php if (!$status) { print ' node-unpublished'; } ?>">
 
 <?php if ($page == 0): ?>
   <h2><a href="<?php print $node_url ?>" title="<?php print $title ?>"><?php print $title ?></a></h2>
